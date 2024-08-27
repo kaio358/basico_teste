70813334d0f261ddc23f20a4ac91173e57ff53d1
Kaio Luiz Ferreira <53657502+kaio358@users.noreply.github.com>
2024-08-27 18:06:06 -0300

    main

--- src/componentes/layout/Menu.js
+++ src/componentes/layout/Menu.js
@@ -1,7 +1,19 @@
+import { useState } from "react";
 import {Link} from "react-router-dom"
 
 import styles from "./Menu.module.css"
+import OpcoesDasCategorias from "../partes/OpcoesDasCategorias";
+
+
 function Menu(){
+
+    const [categoria,setCategoria] = useState(false)
+    const mostrar = ()=>{
+        setCategoria(true)
+    }
+    const esconder = () => {
+        setCategoria(false);
+    };
     return(
         <nav className={styles.menu}>
             <div >
@@ -10,8 +22,12 @@ function Menu(){
             
             </div> 
           
-            <div>
-                <Link to="/filtro">Mais</Link>
+            <div onMouseOver={mostrar} onMouseLeave={esconder} >
+                <Link to="/filtro">Categorias</Link>
+                <div className={`${styles.categorias} ${categoria ? styles.mostrar : ""}`}>
+                    <OpcoesDasCategorias />
+                </div>
+                
             </div>
         </nav>
     )
---
+++ src/componentes/partes/OpcoesDasCategorias.js
@@ -0,0 +1,25 @@
+import styles from "./OpcoesDasCategorias.module.css";
+function OpcoesDasCategorias() {
+  
+    return (
+        <div className={styles.caixaDeOpcoes} >
+            <div>
+                <h4>Coisas</h4>
+                <p>Teste</p>
+                <p>Bonito</p>
+            </div>
+            <div>
+                <h4>Coisas 2</h4>
+                <p>Teste 2</p>
+                <p>Bonito 2</p>
+            </div>
+            <div>
+                <h4>Coisas 3</h4>
+                <p>Teste 3</p>
+                <p>Bonito 3</p>
+            </div>
+        </div>
+    )
+}
+
+export default OpcoesDasCategorias;
