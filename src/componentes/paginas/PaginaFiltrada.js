13c79e572972a6be7d7ab61982accb2f4e833320
Kaio Luiz Ferreira <53657502+kaio358@users.noreply.github.com>
2024-08-29 18:01:11 -0300

    main

--- src/componentes/paginas/PaginaFiltrada.js
+++ src/componentes/paginas/PaginaFiltrada.js
@@ -1,76 +1,64 @@
 import styles from "./PaginaFiltrada.module.css";
-
 import { useEffect, useState } from "react";
-import { useParams,useLocation } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 import queryString from 'query-string';
-
-
 
 import Filtro from "../partes/Filtro";
 import Card from "../partes/Card";
-import Rodape from "../layout/Rodape"
+import Rodape from "../layout/Rodape";
 
+function PaginaFiltrada() {
+    const [dadosLista, setDadosLista] = useState(["bonito", "legal", "teste", "interessante"]);
+    const [resultadoFiltrado, setResultadoFiltrado] = useState([]);
 
-
-function PaginaFiltrada(){
-
-
-    const [dadosLista,setDadosLista] = useState(["bonito","legal","teste","interessante"])
-
-
-    // caso venha na forma de parametro da url de vez do query
-    
-    const {categoria} = useParams()
-    
-    useEffect(()=>{
-        if(categoria){
-            let tratamento = categoria.split(",")
-            console.log(tratamento);
-            let nomes 
-            if(tratamento.length > 1){
-                nomes = tratamento.map(t=> dadosLista.filter(nome=> nome.includes(t))  )
-            }else{
-                nomes = dadosLista.filter(nome=> nome.includes(categoria))
-            }
-            
-           
-           setDadosLista(nomes)
-           
-        }
-    },[categoria])
-
-
+    const { categoria } = useParams();
     const location = useLocation();
 
     useEffect(() => {
         const query = queryString.parse(location.search);
-        const categorias = query.categoria ? query.categoria.split(',') : [];
-        // const cor = query.cor || '';
-        // console.log('Categorias:', categorias);
-        // console.log('Cor:', cor);
-        let nomes  = categorias.map(c =>dadosLista.filter(nome=>nome.includes(c)))
-        setDadosLista(nomes)
-    }, [location.search]);
+        let categorias = [];
+        
+        if (categoria) {
+            categorias = categoria.split(",");
+        } else if (query.categoria) {
+            categorias = query.categoria.split(',');
+        }
 
-    return(
+        if (categorias.length > 0) {
+            let novosResultados = [];
+            categorias.forEach(c => {
+                const resultados = dadosLista.filter(nome => nome.includes(c));
+                novosResultados = [...novosResultados, ...resultados];
+            });
+            setResultadoFiltrado(novosResultados);
+        } else {
+            // Nenhum filtro aplicado, exibir todos os dados
+            setResultadoFiltrado(dadosLista);
+        }
+    }, [categoria, location.search, dadosLista]);
+
+    return (
         <div className={styles.filtroCompleto}>
-           <div className={styles.content}>
-             <div className={styles.espaco}></div>
-             <main className={styles.filtroAmostra}>
-                 <div>
-                     <Filtro/>
-            
-                 </div>
-            
-                 <aside className={styles.ladoDosCards}>
-                     {
-                         dadosLista.map(nome => <Card nome={nome}/>)
-                     }
-                 </aside>
-             </main>
-           </div>
-            <Rodape/>
+            <div className={styles.content}>
+                <div className={styles.espaco}></div>
+                <main className={styles.filtroAmostra}>
+                    <div>
+                        <Filtro />
+                    </div>
+                    <aside className={styles.ladoDosCards}>
+                        {resultadoFiltrado.length > 0 ? (
+                            resultadoFiltrado.map((nome, index) => (
+                                <Card key={index} nome={Array.isArray(nome) ? nome[0] : nome} />
+                            ))
+                        ) : (
+                            <p>Nenhum resultado encontrado.</p>
+                        )}
+                    </aside>
+                </main>
+            </div>
+            <Rodape />
         </div>
-    )
+    );
 }
+
 export default PaginaFiltrada;
